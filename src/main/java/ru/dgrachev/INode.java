82bff5c213076eae436d772bc74161bb404d16f5
Отважный <dimon.fly.mail@mail.ru>
2016-12-13 20:22:54 +0300

    Initial commit

---
+++ src/main/java/ru/dgrachev/INode.java
@@ -0,0 +1,11 @@
+package ru.dgrachev;
+
+/**
+ * Created by OTBA}|{HbIu` on 13.12.16.
+ */
+public interface INode<T extends Object> {
+
+    INode<T> getNextNode();
+    INode<T> getPreviousNode();
+    T getState();
+}
